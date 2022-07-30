3152887650a7e795547d0141eed91f5a445fe770
ISIDLSHTM <103248759+ISIDLSHTM@users.noreply.github.com>
2022-07-30 08:27:32 +0100

    Create create_database.py

---
+++ Running/Objective_3/create_database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+conn = sqlite3.connect('__Objective_3_Database.db')
+c = conn.cursor()
+
+c.execute("""CREATE TABLE experiments (
+            experiment_id int32,
+            method_name str,
+            environment_name str
+            )""")
+
+c.execute("""CREATE TABLE individuals (
+            experiment_id int32,
+            individual_number int16,
+            dose str,
+            efficacy str,
+            toxicity str,
+            predicted_optimal_dose str,
+            actual_utility_at_predicted float32,
+            inaccuracy float32,
+            individual_regret float32,
+            cumulative_regret float32,
+            average_regret float32
+            )""")
+
+conn.close()
